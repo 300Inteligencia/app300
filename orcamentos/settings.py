d3284be427d9768d150f7e939cf4cd7e06366d95
300 Inteligencia <133412199+300Inteligencia@users.noreply.github.com>
2023-05-22 19:36:17 -0300

    Update settings.py

--- orcamentos/settings.py
+++ orcamentos/settings.py
@@ -96,6 +96,7 @@ DATABASES = {
         "PORT": "7352",
         "OPTIONS": {"sslmode": "require"},
         }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
