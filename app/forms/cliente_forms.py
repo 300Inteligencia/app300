9ec788dd8f41488967bf721cbdc56ca32c142ed7
300 Inteligencia <133412199+300Inteligencia@users.noreply.github.com>
2023-10-27 11:22:29 -0300

    Update cliente_forms.py

--- app/forms/cliente_forms.py
+++ app/forms/cliente_forms.py
@@ -17,3 +17,4 @@ class ClienteForm(forms.ModelForm):
 class FiltroMedicoForm(forms.Form):
     medicos_choices = [(choice[0], choice[1]) for choice in Cliente.medico_choices]
     Médico = forms.ChoiceField(choices=[('', 'Todos')] + medicos_choices, required=False)
+    pesquisa = forms.CharField(max_length=100, required=False, label='Paciente')
