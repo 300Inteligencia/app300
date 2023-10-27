4fd6610c6d7d58acdd76b01b328b656020fe2f76
300 Inteligencia <133412199+300Inteligencia@users.noreply.github.com>
2023-10-27 11:21:01 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django_localflavor_br.br_states import STATE_CHOICES
+from django.utils import timezone
 
 # Create your models here.
 
